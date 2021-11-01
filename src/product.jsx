e6b0a7f99c69ff3c65161abf21d3440c17f495bb
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-11-01 22:11:07 +0200

    (15 state image)

--- src/product.jsx
+++ src/product.jsx
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class Product extends React.Component {
     state = {
         name:"Burger",
-        count: 3
+        count: 3,
+        imgUrl :"logo192.png"
     };
     render() { 
         return (
             <div>
+                <img src={this.state.imgUrl} alt="" />
                 <span>{this.state.name}</span>
                 <span className="btn btn-primary m-2">{this.state.count}</span>
             </div>
