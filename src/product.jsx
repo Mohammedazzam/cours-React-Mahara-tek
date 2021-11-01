48a934be04b1cd3b8e0f394ec898052f7c69c11d
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-11-01 21:33:40 +0200

    (13 first react component)

---
+++ src/product.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Product extends React.Component {
+    state = {};
+    render() { 
+        return <span>Product1</span>;
+    }
+}
+
+export default Product;
