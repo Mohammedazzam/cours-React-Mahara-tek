5638ae19cfac5a8607dbbbf8a4f3884fb38458bb
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-11-01 22:40:18 +0200

    (16 etting styles and Classes)

--- src/product.jsx
+++ src/product.jsx
@@ -3,15 +3,36 @@ import React, { Component } from 'react';
 class Product extends React.Component {
     state = {
         name:"Burger",
-        count: 3,
+        count: 0,
         imgUrl :"logo192.png"
     };
-    render() { 
+
+    getClasses() {
+        return  this.state.count === 0
+        ? "btn btn-warning m-2"
+        : "btn btn-primary m-2";
+
+    }
+
+
+    render() {
+        // let classes;
+        //     if(this.state.count === 0 ) {
+        //         classes = 'btn btn-warning m-2'
+        //     }else{
+        //         classes = "btn btn-primary m-2"
+        //     }
+
+            // const classes = this.state.count === 0
+            // ? "btn btn-warning m-2"
+            // : "btn btn-primary m-2";
+
+
         return (
             <div>
-                <img src={this.state.imgUrl} alt="" />
-                <span>{this.state.name}</span>
-                <span className="btn btn-primary m-2">{this.state.count}</span>
+                {/* <img src={this.state.imgUrl} alt="" /> */}
+                <span style={{color:"red"}}>{this.state.name}</span>
+                <span className={this.getClasses()}>{this.state.count}</span>
             </div>
         )
     }
